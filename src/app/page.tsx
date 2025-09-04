019546b7fce7f264d3fae2ae01a38411166fcf37
George Hester <48884343+georgehester@users.noreply.github.com>
2025-09-04 22:13:08 +0100

    fix: auto height boxing for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,21 @@ export default function Home()
 {
     return (
         <div className="flex flex-col grow min-[32rem]:justify-center items-center text-center text-white max-w-5xl mx-auto">
-            {/* <div className="h-72 min-[587px]:h-48 min-[933px]:h-24"> */}
             <div className="m-12">
-
-
-                <TypeAnimation
-                    preRenderFirstString={true}
-                    omitDeletionAnimation={true}
-                    sequence={[
-                        "Leeds Computing Society",
-                        "",
-                        "Leeds Computing Society"
-                    ]}
-                    wrapper="div"
-                    speed={40}
-                    cursor={true}
-                    className="text-3xl min-[32rem]:text-4xl min-[64rem]:text-5xl"
-                />
+                <div className="max-[29.13rem]:h-[9rem] max-[32rem]:h-[6rem] max-[33.63rem]:h-[10.5rem] max-[49.88rem]:h-[7rem] max-[66rem]:h-[3.5rem] h-[4.5rem]">
+                    <TypeAnimation
+                        preRenderFirstString={true}
+                        omitDeletionAnimation={true}
+                        sequence={[
+                            "",
+                            "Leeds Computing Society"
+                        ]}
+                        wrapper="div"
+                        speed={40}
+                        cursor={true}
+                        className="text-header-sm min-[32rem]:text-header-md min-[66rem]:text-header-lg"
+                    />
+                </div>
                 <div className="text-md min-[32rem]:text-lg mt-3">
                     Welcome to the website of the University of Leeds Computing Society
                 </div>
